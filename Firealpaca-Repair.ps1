d28c80ee590170588c012f0cb571bcecd157e329
Atari (Fritz R.) <79813632+b1naryzer0@users.noreply.github.com>
2022-04-22 04:21:24 +0200

    Create Firealpaca-Repair.ps1

---
+++ Firealpaca-Repair.ps1
@@ -0,0 +1,8 @@
+# this repairs FireAlpaca 2.7.7
+# you'll need 
+# https://yurichev.com/utils/PE_patcher64.exe
+# and FireAlpaca ofc
+# after installing FireAlpaca, copy PE_patcher64.exe to your FireAlpaca installation folder
+# open a shell and cd to the folder execute the line below and you're done
+
+PE_patcher64.exe .\FireAlpaca.exe 0x140364A55 909090909090
