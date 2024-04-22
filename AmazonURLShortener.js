f54d5247b51cb5f8a817ec27bfc4c8a6f3e239b2
Atari (Fritz R.) <79813632+b1naryzer0@users.noreply.github.com>
2024-04-22 16:51:58 +0200

    Create AmazonURLShortener.js

---
+++ AmazonURLShortener.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name         Amazon URL Shortener
+// @namespace    https://github.com/r7kamura/amazon_url_shortener_user_script
+// @version      0.2.0
+// @description  Automatically shorten Amazon product URL.
+// @author       forked & modified by Fritz R., original by r7kamura
+// @match      https://smile.amazon.com/*
+// @match      https://www.amazon.ae/*
+// @match      https://www.amazon.au/*
+// @match      https://www.amazon.ca/*
+// @match      https://www.amazon.cn/*
+// @match      https://www.amazon.co.jp/*
+// @match      https://www.amazon.co.uk/*
+// @match      https://www.amazon.com/*
+// @match      https://www.amazon.com.br/*
+// @match      https://www.amazon.com.mx/*
+// @match      https://www.amazon.com.tr/*
+// @match      https://www.amazon.de/*
+// @match      https://www.amazon.es/*
+// @match      https://www.amazon.fr/*
+// @match      https://www.amazon.in/*
+// @match      https://www.amazon.it/*
+// @match      https://www.amazon.nl/*
+// @match      https://www.amazon.sa/*
+// @match      https://www.amazon.se/*
+// @match      https://www.amazon.sg/*
+// ==/UserScript==
+
+(function () {
+  function replaceState(path) {
+    window.history.replaceState({}, "", path);
+  }
+
+  const asinElement = document.querySelector(
+    '#ASIN, input[name="idx.asin"], input[name="ASIN.0"], input[name="titleID"]'
+  );
+
+  if (asinElement) {
+    replaceState(`/dp/${asinElement.value}`);
+  } else {
+    const href = window.location.href;
+    const match = href.match(/\/(?:dp|gp\/product)\/([A-Z0-9]{10})[/?]?/);
+    if (match) {
+      replaceState(`/dp/${match[1]}`);
+    }
+  }
+})();
